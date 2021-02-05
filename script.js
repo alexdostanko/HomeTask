9fe7a12d0a5ef4779c7e91d37275090bd2d376a4
Alexandra Dostanko <78319071+alexdostanko@users.noreply.github.com>
2021-02-05 17:29:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector('.block').addEventListener('click',function() {
 	this.classList.toggle("night");
 });
+
+document.querySelector('.ssylka').addEventListener('click',function() {
+	window.location.href = "https://pzz.by/";
+});
